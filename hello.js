77dd6c884a726b5c24a6912c519df1b2125f81d3
Duc-Hoang Pham <75479950+hoangpanda@users.noreply.github.com>
2023-09-11 14:05:59 +0700

    add comment hello.js

--- hello.js
+++ hello.js
@@ -1,15 +1,18 @@
+# define some const :D 
 const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
+# handle request 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end("Welcome to my website :DDDD");
+    res.end("Welcome to my website :DDDD);
 };
 
-const server = http.createServer(requestListener);
+# create server
+const server = http.createServer(requestListener); 
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+    console.log(`Server is running on http://${host}:${port}`); # console log in terminal when run file
 });
 
-
+# 22021200 Pham Duc Hoang
